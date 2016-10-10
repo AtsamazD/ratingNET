4a849306e596c648b3714469b8411d896d6f3eac
Atsamaz Datiev <fess61@ya.ru>
2016-10-10 10:34:16 +0300

    Preview!

--- kafrating/login.aspx
+++ kafrating/login.aspx
@@ -7,6 +7,11 @@
     <link rel="stylesheet" href="css/style.css" type="text/css"/>
 	<link rel="stylesheet" href="css/framework.css" type="text/css"/>
     <title>Рейтинг кафедр | СКГМИ</title>
+    <style type="text/css">
+        #filesList{
+            vertical-align: top;
+        }
+    </style>
 </head>
 <body class="g">
     <div class="header f-bwi g-row">
@@ -24,16 +29,31 @@
     </div>
     <form id="form1" runat="server" class="g-row">
         <div id="loginForm">
-            <h2>Выберите файл с критериям</h2>
-            <asp:FileUpload ID="FileUpload1" runat="server"  /> &nbsp <asp:TextBox ID="newFileName" runat="server"></asp:TextBox> <asp:Button ID="Button1" CssClass="f-bu" runat="server" Text="Загрузить"  OnClick="Button1_Click"/>
-            <asp:Label ID="UploadStatus" runat="server" Text=""></asp:Label>
-            <br /><br />
-            <asp:Label ID="LabelFilesList" runat="server" Text="Список файлов на сервере" Visible="False"></asp:Label>
-            <asp:ListBox ID="filesList" runat="server" Visible="False"></asp:ListBox>
+            <h2>Выберите файл с критериями</h2>
+            <table>
+                <tr>
+                    <td><asp:FileUpload ID="FileUpload1" runat="server"  /></td>
+                    <td><asp:Button ID="Button1" CssClass="f-bu" runat="server" Text="Загрузить"  OnClick="Button1_Click"/>&nbsp<asp:Label ID="UploadStatus" runat="server" Text=""></asp:Label></td>
+                </tr>
+                <tr>
+                    <td><asp:Label ID="LabelFilesList" runat="server" Text="Список файлов на сервере"></asp:Label></td>
+                    <td><asp:ListBox ID="filesList" runat="server" Height="100px"></asp:ListBox>                        
+                    </td>
+                </tr>
+                <tr>
+                    <td><asp:Button ID="xlsButton" CssClass="f-bu" runat="server" Text="Подгрузить значение" OnClick="xlsButton_Click" /></td>
+                    <td></td>
+                </tr>
+            </table>          
             <br />
-            <asp:Button ID="DeleteFiles" CssClass="f-bu" runat="server" Text="Очистить директорию" OnClick="DeleteFiles_Click" Visible="False" />
+            <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+            <hr />
+            <asp:Label ID="infoLabel" runat="server" Text="Техническая информация: " Font-Names="Consolas"></asp:Label>    
+            <br />
+            <asp:TextBox ID="infoBox" TextMode="MultiLine" CssClass="g-12" runat="server" Height="100px" Font-Names="Consolas" ReadOnly="True"></asp:TextBox>
         </div>        
         <hr />
+        
         <a href="index.html">Выход</a>
     </form>
 </body>
--- kafrating/login.aspx.cs
+++ kafrating/login.aspx.cs
@@ -5,60 +5,141 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.IO;
 
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Data;
+
 public partial class kafrating_login : System.Web.UI.Page
 {
-    string s1 = @"\\sql\www\Rating\upload\", s2 = @"D:\Документы\Rating.net\upload\";
+    string s1 = @"D:\wwwroot\Rating\upload\", s2 = @"D:\Документы\Rating.net\upload\";
     string[] files;
-    bool ExistFile;
-   
+
+    void loadList(string path)
+    {
+        try
+        {
+            filesList.Items.Clear();
+            files = Directory.GetFiles(path);
+            filesList.Width = 200;
+            filesList.Height = 100;
+            for (int i = 0; i < files.Length; i++)
+                filesList.Items.Add(Path.GetFileName(files[i]));
+        }
+        catch (Exception msg)
+        {
+            MSG(msg.Message);
+        }
+    }
+    void MSG(string msg)
+    {
+        infoBox.Text += DateTime.Now.Hour.ToString("00") + ":" + DateTime.Now.Minute.ToString("00") + ":" + DateTime.Now.Second.ToString("00") + "  " + msg + Environment.NewLine;
+    }
     protected void Button1_Click(object sender, EventArgs e)
     {
-        if (!ExistFile)
-        {
-            string s1 = @"\\sql\www\Rating\upload\", s2 = @"D:\Документы\Rating.net\upload\";
-            string savePath = s2;
+        string savePath = s2;
 
-            if (FileUpload1.HasFile)
+        if (FileUpload1.HasFile)
+        {
+            try
             {
                 string filename = FileUpload1.FileName;
-                savePath += filename;
-                ExistFile = false;
-                for (int i = 0; i < files.Length; i++)
-                    if (Path.GetFileName(files[i]) == filename)
-                    {
-                        ExistFile = true;
-                        break;
-                    }
-                if (ExistFile)
+                if ((Path.GetExtension(filename) == ".xlsx") || (Path.GetExtension(filename) == ".xls"))
                 {
-                    UploadStatus.Text = "Файл с таким названием уже существует, переименуете?";
-                    newFileName.Text = filename;
+                    savePath += filename;
                     FileUpload1.SaveAs(savePath);
-                    UploadStatus.Text = "Файл " + newFileName.Text + " загружен";
+                    loadList(s2);
                 }
                 else
-                {
-                    FileUpload1.SaveAs(savePath);
-                    UploadStatus.Text = "Файл " + filename + " загружен";
-                }
+                    MSG("Выбранный файл не имеет расширения .xlsx или .xls!");
             }
-            else
-                UploadStatus.Text = "Не указан файл для загрузки!";
+            catch (Exception msg)
+            {
+                MSG(msg.Message);
+            }
         }
         else
-        {
-
-        }
+            UploadStatus.Text = "Не указан файл для загрузки!";
     }
     protected void Page_Load(object sender, EventArgs e)
     {
-        filesList.Items.Clear();
-        files = Directory.GetFiles(s2);
-        for (int i = 0; i < files.Length; i++)
-            filesList.Items.Add(Path.GetFileName(files[i]));
+        loadList(s2);
     }
-    protected void DeleteFiles_Click(object sender, EventArgs e)
-    {
 
+    protected void xlsButton_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            Excel.Application xlApp;
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+            Excel.Range ShtRange;
+
+            DataTable dt = new DataTable();
+
+            object misValue = System.Reflection.Missing.Value;
+
+            xlApp = new Excel.Application();
+            xlWorkBook = xlApp.Workbooks.Open(s2 + filesList.Items[filesList.SelectedIndex + 1].Text, 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            ShtRange = xlWorkSheet.UsedRange;
+
+            for (int Cnum = 1; Cnum <= ShtRange.Columns.Count; Cnum++)
+            {
+                dt.Columns.Add(
+                   new DataColumn((ShtRange.Cells[1, Cnum] as Excel.Range).Value2.ToString()));
+            }
+            dt.AcceptChanges();
+
+            string[] columnNames = new String[dt.Columns.Count];
+            for (int i = 0; i < dt.Columns.Count; i++)
+            {
+                columnNames[0] = dt.Columns[i].ColumnName;
+            }
+
+            for (int Rnum = 2; Rnum <= ShtRange.Rows.Count; Rnum++)
+            {
+                DataRow dr = dt.NewRow();
+                for (int Cnum = 1; Cnum <= ShtRange.Columns.Count; Cnum++)
+                {
+                    if ((ShtRange.Cells[Rnum, Cnum] as Excel.Range).Value2 != null)
+                    {
+                        dr[Cnum - 1] =
+            (ShtRange.Cells[Rnum, Cnum] as Excel.Range).Value2.ToString();
+                    }
+                }
+                dt.Rows.Add(dr);
+                dt.AcceptChanges();
+            }
+
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+            xlApp.Quit();
+
+            releaseObject(xlWorkSheet);
+            releaseObject(xlWorkBook);
+            releaseObject(xlApp);
+        }
+        catch (Exception msg)
+        {
+            MSG(msg.Message);
+        }
+    }
+
+    private void releaseObject(object obj)
+    {
+        try
+        {
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+            obj = null;
+        }
+        catch (Exception ex)
+        {
+            obj = null;
+            MSG("Unable to release the Object " + ex.ToString());
+        }
+        finally
+        {
+            GC.Collect();
+        }
     }
 }
